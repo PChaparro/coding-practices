0f786a5c83cc7853c8b55d2db3ec6ef62d55c401
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-04-08 19:12:53 -0500

    chore: Add 'c++' base file

---
+++ templates/cpp/template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utils
+// #define ll long long
+// #define lld long double
+// #define umap unordered_map
+// #define uset unordered_set
+// typedef pair<int,int> pii;
+
+// Whether the input has multiple test cases
+bool multi = true;
+
+void solve()
+{
+}
+
+int main()
+{
+  // Optimize IO
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  // Handle multiple test cases
+  int t = 1;
+  if (multi)
+    cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
